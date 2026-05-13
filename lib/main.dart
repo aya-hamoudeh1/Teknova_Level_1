296db1529e3c6a042d503e1de7a2188879c3d022
Aya Hamoudeh <ayahamoudeh29@gmail.com>
2026-05-13 23:32:02 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Learning"),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          leading: Icon(Icons.arrow_back_ios),
+        ),
+
+        body: Column(
+          mainAxisAlignment: .center,
+          children: [
+            Text(
+              "Flutter Course is Starting",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            ),
+
+            SizedBox(height: 28),
+
+            Container(
+              padding: EdgeInsets.all(16),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(color: Colors.blue, width: 2),
+              ),
+              child: Text(
+                'fghbjn jjhkl; jhkds hjkl,,  kkjoijds   jjhijds fghbjn jjhkl; jhkds hjkl,,  kkjoijds   jjhijds ',
+                textAlign: .center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 28),
+
+            Row(
+              mainAxisAlignment: .center,
+                children: [
+              Icon(Icons.star, color: Colors.orange),
+              Text("dfghjk")
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
